6b6302ec48b99989528b560e0b5cca27e3da1561
atharva009 <atharvahankare@gmail.com>
2025-01-04 19:24:02 -0500

    add Header

---
+++ frontend/src/Header/NavLinks.tsx
@@ -0,0 +1,12 @@
+const NavLinks = () => {
+  return (
+    <div className="flex gap-10">
+      <a href="">Find Jobs</a>
+      <a href="">Find Talent</a>
+      <a href="">Post Job</a>
+      <a href="">About Us</a>
+    </div>
+  );
+};
+
+export default NavLinks;
