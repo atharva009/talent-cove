e3df6e820328255177b6c02d8a4b6ca348fa0c4c
atharva009 <atharvahankare@gmail.com>
2025-01-14 20:05:11 -0500

    add landing page top

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import Header from "../Header/Header";
+import DreamJob from "../Landing/DreamJob";
 
 const HomePage = () => {
     return (
-        <div className="min-h[100vh] bg-mine-shaft-950 font-[Poppins]">
+        <div className="min-h-[100vh] bg-mine-shaft-950 font-[Poppins]">
             <Header />
+            <DreamJob />
         </div>
     )
 }
