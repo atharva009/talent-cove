6fa6f8d3892dc4bfc5562f22408701531f07e034
atharva009 <atharvahankare@gmail.com>
2025-02-15 15:48:10 -0500

    feat: backend init

---
+++ backend/src/main/java/com/talentcove/TalentCoveApplication.java
@@ -0,0 +1,13 @@
+package com.talentcove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentCoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentCoveApplication.class, args);
+	}
+
+}
